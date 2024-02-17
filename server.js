efb03cd1187b86ef3714c4b5fea2a90e53306e62
Derick Estephen <derickestephen1999@gmail.com>
2024-02-17 15:53:21 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dbConnect = require('./database/index');
+const {PORT} = require('./config/index');
+const app = express();
+
+dbConnect();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Backend is running on port ${PORT}`);
+})
